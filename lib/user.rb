715c1a2ad17458ea8780192c9aa12c5924ecfd24
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-22 18:52:06 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,9 @@
 class User
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :knowledge
   def intialize
     @first_name = nil 
     @last_name = nil
-    
+    @knowledge = []
   end
   
 end
